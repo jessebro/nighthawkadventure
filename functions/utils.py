ce24e1204441b81ae8f7f2a5091639470793b55d
Rick <ricksbrown73@gmail.com>
2021-05-20 21:09:11 +1000

    Add colored print function

--- functions/utils.py
+++ functions/utils.py
@@ -1,4 +1,9 @@
 from functions import loading
+import os
+
+# System call - apparently needed for color, see
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+os.system('')
 
 
 def boot():
@@ -23,3 +28,25 @@ def input_stuff(prompt, options):
 		choice = input(prompt)
 		if choice in options:
 			return choice
+
+
+# Usage: print_it('Hello', 'RED')
+def print_it(msg, color='RESET'):
+	color = color.upper()
+	if color not in style:
+		color = 'RESET'
+	print(style[color] + msg + style['RESET'])
+
+
+style = {
+	'BLACK': '\033[30m',
+	'RED': '\033[31m',
+	'GREEN': '\033[32m',
+	'YELLOW': '\033[33m',
+	'BLUE': '\033[34m',
+	'MAGENTA': '\033[35m',
+	'CYAN': '\033[36m',
+	'WHITE': '\033[37m',
+	'UNDERLINE': '\033[4m',
+	'RESET': '\033[0m',
+}
