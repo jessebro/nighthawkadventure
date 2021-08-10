c18ddb087f0f8ddb18d3ba4ad4374e28df8b7839
Jesse Brown <jesseatbrown@gmail.com>
2021-08-10 19:45:09 +1000

    Adding achievements

--- functions/achievments.py
+++ functions/achievments.py
@@ -1,2 +1,21 @@
+achievements = {
+	"resourceful": {
+		"name": "Resourceful.",
+		"description": "Complete a combat encounter without using items.",
+		"completed": "You completed a combat encounter without using items.",
+		"unlocked": False
+	},
+	"untouchable": {
+		"name": "Untouchable.",
+		"description": "Complete a combat encounter of three or more enemies without taking damage.",
+		"completed": "You complete a combat encounter of three or more enemies without taking damage.",
+		"unlocked": False
+	},
+	"body_count": {
+		"name": "Body Count.",
+		"description": "Defeat a group of two or more enemies in the same amount of turns.",
+		"completed": "You defeated a group of two or more enemies in the same amount of turns.",
+		"unlocked": False
+	}
 
-
+}
