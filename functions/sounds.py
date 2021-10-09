43a6af233f88e10c44a751fdb280b18180caa976
Jesse Brown <jesseatbrown@gmail.com>
2021-10-09 19:02:14 +1100

    Made music better

--- functions/sounds.py
+++ functions/sounds.py
@@ -8,192 +8,94 @@ settings.load_settings()
 mixer.init()
 mixer.music.set_volume(settings.settings["music"] / 10)
 
+
 def get_volume():
 	volume = settings.check_settings()
 	volume = volume["music"]
 	return volume
 
+
 def play_combat(type):
-	global playing
 	if type == "human":
-		play = "music/Sworddancers.ogg"
+		song = "music/Sworddancers.ogg"
 	else:
-		play = "music/Song of the Nighthawk.ogg"
-	if playing == "fighty":
-		return
-	playing = "fighty"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load(play)
-	mixer.music.play(-1)
+		song = "music/Song of the Nighthawk.ogg"
+	play(song)
 
 
 def end_combat():
-	global playing
-	if playing == "music/Combat_End.ogg":
-		return
-	playing = "music/Combat_End.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.stop()
-	mixer.music.load('music/Combat_End.ogg')
-	mixer.music.play()
+	play_combat_end("music/Combat_End.ogg")
 
 
 def end_human():
-	global playing
-	if playing == "music/Sworddancers end.ogg":
-		return
-	playing = "music/Sworddancers end.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.stop()
-	mixer.music.load('music/Sworddancers end.ogg')
-	mixer.music.play()
+	play_combat_end('music/Sworddancers end.ogg')
 
 
 def dungeon():
-	global playing
-	if playing == "music/Forsaken Halls.ogg":
-		return
-	playing = "music/Forsaken Halls.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Forsaken Halls.ogg')
-	mixer.music.play(-1)
+	play("music/Forsaken Halls.ogg")
 
 
 def title():
-	global playing
-	if playing == "music/A Blade and a Bag.ogg":
-		return
-	playing = "music/A Blade and a Bag.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/A Blade and a Bag.ogg')
-	mixer.music.play(-1)
+	play("music/A Blade and a Bag.ogg")
 
 
 def travel():
-	global playing
-	if playing == "music/The Winding Road.ogg":
-		return
-	playing = "music/The Winding Road.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/The Winding Road.ogg')
-	mixer.music.play(-1)
+	play("music/The Winding Road.ogg")
 
 
 def town():
-	global playing
-	if playing == "music/City of Sails.ogg":
-		return
-	playing = "music/City of Sails.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/City of Sails.ogg')
-	mixer.music.play(-1)
+	play("music/City of Sails.ogg")
 
 
 def night():
-	global playing
-	if playing == "music/A Deep Sky.ogg":
-		return
-	playing = "music/A Deep Sky.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/A Deep Sky.ogg')
-	mixer.music.play(-1)
+	play("music/A Deep Sky.ogg")
 
 
 def dire():
-	global playing
-	if playing == "music/Dire Circumstances.ogg":
-		return
-	playing = "music/Dire Circumstances.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Dire Circumstances.ogg')
-	mixer.music.play(-1)
+	play("music/Dire Circumstances.ogg")
 
 
 def doom():
-	global playing
-	if playing == "music/Approaching Doom.ogg":
-		return
-	playing = "music/Approaching Doom.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Approaching Doom.ogg')
-	mixer.music.play(-1)
+	play("music/Approaching Doom.ogg")
 
 
 def end_boss():
-	global playing
-	if playing == "music/Boss_End.ogg":
-		return
-	playing = "music/Boss_End.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.stop()
-	mixer.music.load('music/Boss_End.ogg')
-	mixer.music.play()
+	play_combat_end("music/Boss_End.ogg")
 
 
 def jungle():
-	global playing
-	if playing == "music/Jungle of Mysteries.ogg":
-		return
-	playing = "music/Jungle of Mysteries.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Jungle of Mysteries.ogg')
-	mixer.music.play(-1)
+	play("music/Jungle of Mysteries.ogg")
 
 
 def fairy():
-	global playing
-	if playing == "music/Whispering Fairies.ogg":
-		return
-	playing = "music/Whispering Fairies.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Whispering Fairies.ogg')
-	mixer.music.play(-1)
+	play("music/Whispering Fairies.ogg")
 
 
 def village():
-	global playing
-	if playing == "music/Haven of Humanity.ogg":
-		return
-	playing = "music/Haven of Humanity.ogg"
-	pygame.init()
-	mixer.init()
-	mixer.music.fadeout(500)
-	mixer.music.load('music/Haven of Humanity.ogg')
-	mixer.music.play(-1)
+	play("music/Haven of Humanity.ogg")
 
 
 def level_up():
+	play("music/Level Up.ogg", loop=1)
+
+
+def play_combat_end(song):
+	play(song, loop=1, fade=0)
+
+
+def play(song, loop=-1, fade=500):
 	global playing
+	combat_songs = ('music/Sworddancers.ogg', 'music/Song of the Nighthawk.ogg')
+	if song in combat_songs and playing in combat_songs:
+		return
+	if song != "music/Level Up.ogg":
+		playing = song
 	pygame.init()
 	mixer.init()
-	mixer.stop()
-	mixer.music.load('music/Level Up.ogg')
-	mixer.music.play()
+	mixer.music.fadeout(fade)
+	mixer.music.load(song)
+	mixer.music.play(loop)
 
 
 def play_prev():
-	mixer.music.load(playing)
-	mixer.music.play(-1)
+	play(playing, fade=0)
