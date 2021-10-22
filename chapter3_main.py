16a6d5a234ea610f2bd75e5065ead401d75cfa20
Jesse Brown <jesseatbrown@gmail.com>
2021-10-22 12:19:58 +1100

    I finally got it to work

--- chapter3_main.py
+++ chapter3_main.py
@@ -3,7 +3,6 @@ from functions.loading import save
 
 def part_one():
 	save('chapter3_main.part_one')
-	# file = character.get_crossroads("1")
-	file = "ch"
+	file = __import__("chapter3_jungle")
 	run = getattr(file, "part_one")
 	run()
