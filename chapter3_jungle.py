44799ac94165037746e9dd70026023dba324cc4d
Jesse Brown <jesseatbrown@gmail.com>
2021-10-22 12:25:01 +1100

    All set up for a big story push

--- chapter3_jungle.py
+++ chapter3_jungle.py
@@ -1,2 +1,21 @@
+import random
+from functions import ability
+from functions import equipment
+from functions import character
+from functions import encounters
+from functions import enemy_round
+from functions import weapon
+from functions import rest
+from functions.utils import print_stuff
+from functions.utils import input_stuff
+from functions.utils import colour_it
+from functions.utils import Color
+from functions.utils import clear
+from functions.loading import save
+from functions import town
+from functions import sounds
+from functions.character import story
+
+
 def part_one():
 	print("Hola")
